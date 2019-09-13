0b7978419d5d125572625e7be9c0b95b385ea753
khadija <ndiaye1kadidiatou@gmail.com>
2019-09-13 16:36:34 +0000

    Good authentification

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,557 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\Compts;
+use App\Form\UserType;
+use App\Form\ComptType;
+use App\Form\LoginType;
+use App\Form\BloquerType;
+use App\Entity\Partenaire;
+use App\Form\ComptuserType;
+use App\Form\PartenaireType;
+use App\Repository\UserRepository;
+use App\Repository\ComptsRepository;
+use App\Repository\PartenaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+
+   /**
+    * @Route("/api")
+   */
+class SecurityController extends AbstractController
+
+{
+
+    /**
+     * @Route("/listpart", name="listpart" , methods={"POST", "GET"})
+     * @IsGranted("ROLE_SUPERADMIN")
+     */
+    public function listpart( PartenaireRepository $partRepository , SerializerInterface $serializer): Response
+    {
+        $liste = $partRepository->findAll();
+        $data = $serializer->serialize($liste, 'json', [
+            'groups' => ['list']
+        ]);
+        return new Response($data, 200, [
+
+            'Content-Type' => 'application/json'
+        ]);
+       
+    }
+
+     /**
+     * @Route("/listuserpart", name="listuserpart" , methods={"POST", "GET"})
+     * @IsGranted({"ROLE_SUPERADMIN", "ROLE_ADMIN"})
+     */
+    public function listuserpart(UserRepository $userRepository , SerializerInterface $serializer): Response
+    {   $partuser=$this->getUser()->getPartenaire();
+        $liste = $userRepository->findBy(["partenaire" => $partuser]);
+        $data = $serializer->serialize($liste, 'json', [
+            'groups' => ['list']
+        ]);
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+       
+    }
+
+    /**
+     * @Route("/listuser", name="listuser" , methods={"POST", "GET"})
+     * @IsGranted("ROLE_SUPERADMIN")
+     * 
+     
+     */
+    public function listUser( UserRepository $userRepository , SerializerInterface $serializer): Response
+    {
+        $listeuser = $userRepository->findAll();
+        $data = $serializer->serialize($listeuser, 'json', [
+            'groups' => ['list']
+        ]);
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+       
+    }
+    /**
+     * @Route("/listcompt", name="listcompt",methods={"POST","GET"})
+     * @IsGranted({"ROLE_SUPERADMIN", "ROLE_ADMIN"})
+     */
+    public function listcompt(ComptsRepository $comptRepository , SerializerInterface $serializer): Response
+    {
+        $compte =$this->getUser()->getPartenaire();
+        $listecompt= $comptRepository->findBy(['partenaire' => $compte]);
+        $data = $serializer->serialize($listecompt, 'json', [
+            'groups' => ['list']
+        ]);
+        return new Response($data, 200, [
+
+            'Content-Type' => 'application/json'
+        ]);
+    }
+    /**
+     * @Route("/comptAll", name="listcomptall",methods={"POST","GET"})
+     * @IsGranted("ROLE_SUPERADMIN")
+     * 
+     */
+    public function compteAll(ComptsRepository $comptRepository , SerializerInterface $serializer): Response
+    {
+        $listcompte = $comptRepository->findAll();
+        $data = $serializer->serialize($listcompte, 'json', [
+            'groups' => ['list']
+        ]);
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    private $passwordEncoder;
+
+public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+{
+  $this->passwordEncoder = $passwordEncoder;
+}
+    /**
+     * @Route("/login12", name="login12", methods={"POST"})
+     * @param JWTEncoderInterface $JWTEncoder
+     * @throws \Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTEncodeFailureException
+     */
+    public function login(Request $request, JWTEncoderInterface $JWTEncoder)
+    {
+        $values = json_decode($request->getContent());
+        $username   = $values->username;
+        $password   = $values->password;
+        
+
+            $repo = $this->getDoctrine()->getRepository(User::class);
+            $user = $repo-> findOneBy(['username' => $username]);
+            if(!$user){
+                return $this->json([
+                        'alerte1' => 'Username incorrect'
+                    ]);
+            }
+
+            $isValid = $this->encoder
+            ->isPasswordValid($user, $password);
+            if(!$isValid){ 
+                return $this->json([
+                    'alerte2' => 'Mot de passe incorect'
+                ]);
+            }
+            if($user->getStatus()!=null && $user->getStatus()=="bloquer"){
+                return $this->json([
+                    'Message1' => 'Acces refusé!! Vous etes bloquer,Veillez contacter votres
+                    administrateur !'
+                ]);
+            }
+            elseif($user->getPartenaire()!=NULL && $user->getPartenaire()->getStatus()=="bloquer"){
+                return $this->json([
+                    'Message2' => 'Acces refusé!! Vous etes bloquer,Veillez contacter le service client'
+                ]);
+            }
+            else {
+                $token = $JWTEncoder->encode([
+                    'username' => $user->getUsername(),
+                    'partenaire' => $user->getPartenaire(),
+                    //'roles'=> $user->getRoles(),
+                    'exp' => time() + 3600 // 1 hour expiration
+                ]);
+    
+                return $this->json([
+                    'token' => $token
+                ]);
+            }
+        
+                
+   
+        
+    }
+
+//  /**
+//      * @Route("/login1", name="login1", methods={"POST"})
+//      * @param JWTEncoderInterface $JWTEncoder
+//      * @throws \Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTEncodeFailureException
+//      */
+//     public function login1(Request $request, JWTEncoderInterface $JWTEncoder,UserRepository $repo){
+//        $user =  new User();
+//       // $values=$request->request->all();//si form
+//       $values =json_decode($request->getContent(),true);
+//     $form = $this->createForm(LoginType::class, $user);
+//     $form->handleRequest($request);
+//    // $form->submit($values);
+//     $user = $repo->findOneBy([
+//          'username' =>$values["username"]
+//     ]); 
+
+//    // return new JsonResponse(['user'=>$user]);
+
+//     /*if (!$user) {
+//         //throw $this->createNotFoundException('usermane invalide');
+//         $data = [
+            
+//             'alert1' => 'Username incorrect'
+//         ];
+//         return new JsonResponse($data);
+//     }
+//     $isValid = $this->passwordEncoder->isPasswordValid($user, $values["password"]);
+  
+   
+//     if(!$isValid){ 
+//      //throw $this->createNotFoundException('Password incorrect');
+//      $data = [
+            
+//         'alert2' => 'Password incorrect'
+//     ];
+//     return new JsonResponse($data);
+
+//     }
+    
+//     if($user->getStatus()=="bloquer"){
+//        // throw $this->createNotFoundException('Acces refuser! Veillez contacter l admin');
+//         $data = [
+            
+//             'Message1' => 'Acces refusé!! Vous etes bloquer,Veillez contacter votres
+//              administrateur'
+//         ];
+//         return new JsonResponse($data);
+//     }
+//     elseif($user->getPartenaire()!=NULL && $user->getPartenaire()->getStatus()=="bloquer"){
+//         //throw $this->createNotFoundException('Acces refuser! Veillez contacter l superadmin');
+//         $data = [
+          
+//             'Message2' => 'Acces refusé!! Vous etes bloquer,Veillez contacter le service
+//             client'
+//         ];
+
+//         return new JsonResponse($data);
+//     }else{
+//         $token = $JWTEncoder->encode([
+//             'username' => $user->getUsername(),
+//             'partenaire' => $user->getPartenaire(),
+//             'roles'=> $user->getRoles(),
+//             'exp' => time() + 3600 // 1 hour expiration
+//         ]);
+
+//     return new JsonResponse(['token' => $values["username"],
+//                              'user' =>  $values["username"]
+//                              ]);        
+//         }*/
+//         $login= $user->getUsername();
+//         return new JsonResponse(['token' =>  $values
+                             
+//                              ]);  
+//     }
+
+
+
+    /**
+     * @Route("/register", name="register", methods={"POST"})
+     * @IsGranted("ROLE_SUPERADMIN")
+
+     */
+
+    //-------Ajout d'un SupertUser et Caissier----/////
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $entityManager, ValidatorInterface $validator)
+    {
+         $values=$request->request->all();//si form
+     
+        $file=$request->files->all()['imageName'];
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+       
+        $form->submit($values);
+     
+        if ($form->isSubmitted() && $form->isValid()) {
+        
+         $user->setPassword(
+            $passwordEncoder->encodePassword(
+                $user,
+                $form->get('password')->getData()
+            )
+        );           
+            if ($values['profil']==1) {
+                $user->setRoles(['ROLE_SUPERADMIN']);    
+            }
+           
+            if ($values['profil']==2) {
+                $user->setRoles(['ROLE_CAISSIER']);    
+            }
+            $user->setImageFile($file);  
+            $user->setUpdatedAt(new \DateTime()) ; 
+            $user->setStatus('Activer');
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $data = [
+                'statut' => 201,
+                'Message' => 'L\'utilisateur ajouter'
+            ];
+
+            return new JsonResponse($data, 201);
+        }
+        return new Response($validator->validate($form));
+    }
+    /**
+     * @Route("/ajoutpart", name="ajoutpart", methods={"POST"}) 
+     * @IsGranted("ROLE_SUPERADMIN")
+
+     */
+     
+     //-------Ajout d'un Partenaire et son Admin et Compt ----/////
+     public function ajoutpart(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $entityManager , ValidatorInterface $validator)
+     {
+         $values=$request->request->all();//si form
+ 
+         $file=$request->files->all()['imageName'];
+ 
+         $part = new Partenaire();
+ 
+         $form=$this->createForm(PartenaireType::class, $part);
+ 
+         $form->handleRequest($request);
+        
+         $form->submit($values);
+ 
+          $part->setStatus('Activer');
+           
+          $compt = new Compts();
+ 
+          // Enregistrons les informations de date dans des variables
+          $form=$this->createForm(ComptType::class, $compt);
+          $form->handleRequest($request);
+         
+          $form->submit($values);
+                  $jours = date('d');
+                  $mois = date('m');
+                  $annee = date('Y');
+          
+                 $heure = date('H');
+                 $minute = date('i');
+                 $seconde= date('s');
+      $test = $jours.$mois.$annee.$heure.$minute.$seconde;
+          $compt->setNumcompt($test);
+          $compt->setPartenaire($part);
+          $compt->setSolde(0);
+ 
+ 
+          $user = new User();
+         $form = $this->createForm(UserType::class, $user);
+         $form->handleRequest($request);
+                   
+         $form->submit($values);
+                 
+          $user->setPassword($passwordEncoder->encodePassword($user,
+             $form->get('password')->getData())); 
+ 
+          if ($values['profil']==3) {
+              $user->setRoles(['ROLE_ADMIN']);    
+                      }
+                       
+         if ($values['profil']==4) {
+             $user->setRoles(['ROLE_USER']);    
+                   }
+             $user->setUpdatedAt(new \DateTime()) ; 
+             $user->setStatus('Activer');
+                  
+             $user->setImageFile($file); 
+             $user->setStatus('Activer');
+             $user->setPartenaire($part);
+             $user->setNumcompt($compt);
+            
+             $entityManager = $this->getDoctrine()->getManager();
+ 
+             $entityManager->persist($user);
+             $entityManager->persist($part);
+             $entityManager->persist($compt);
+             $entityManager->flush();
+     
+             $data = [
+                 'statut' => 201,
+                 'Messages' => 'Le partenaire ajouter'
+             ];
+ 
+             return new JsonResponse($data, 201);
+
+             //return new Response($validator->validate($form));
+ 
+            }
+    /**
+     * @Route("/ajoutpartuser", name="ajoutpartuser", methods={"POST"})
+     * @IsGranted("ROLE_ADMIN")
+     
+     */
+     //-------Ajout des users d'un partenaire  ----/////
+    public function ajoutpartuser(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $entityManager, ValidatorInterface $validator)
+    {
+    
+     $values=$request->request->all();//si form
+     $file=$request->files->all()['imageName'];
+     $user = new User();
+     $form = $this->createForm(UserType::class, $user);
+     $form->handleRequest($request);
+      
+     $form->submit($values);
+    
+       if ($form->isSubmitted()) {
+       
+        $user->setPassword(
+           $passwordEncoder->encodePassword(
+               $user,
+               $form->get('password')->getData()
+           )
+       );           
+    if ($values['profil']==3) {
+        $user->setRoles(['ROLE_ADMIN']);    
+                }
+                 
+   if ($values['profil']==4) {
+       $user->setRoles(['ROLE_USER']);    
+             }
+
+     $partenaire=$this->getUser()->getPartenaire();
+
+            $user->setPartenaire($partenaire);
+            $user->setStatus('Activer');
+            $user->setImageFile($file);
+
+            $user->setUpdatedAt(new \DateTime()) ;
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $data = [
+                'statu' => 201,
+                'messages' => 'Lutilisateur ajouter'
+            ];
+
+            return new JsonResponse($data, 201);
+   }
+   return new Response($validator->validate($form));
+
+}
+//-------Bloquer Debloquer users part-----------------/////
+
+/**
+ * @Route("/bloquer/{id}", name="userBlock", methods={"GET","POST","PUT"})
+ * @IsGranted({"ROLE_SUPERADMIN", "ROLE_ADMIN"})
+ */
+
+public function userBloquer(User $users, Request $request, UserRepository $userRepo,EntityManagerInterface $entityManager): Response
+    {
+
+        // $values=$request->request->all();//si form
+        // $user = new User();
+        // $form = $this->createForm(BloquerType::class, $user);
+        // $form->handleRequest($request);
+         
+        // $form->submit($values);
+        $values = json_decode($request->getContent(),true);
+
+        $user=$userRepo->find($users->getId());
+
+        //$user = $entityManager->getRepository(User::class)->find($user->getId());
+       // $user=$userRepo->findOneByUsername($values['username']);
+        
+       if($user->getUsername()== "cheikh"){
+            
+        return $this->json([
+            'message1' =>'Attention cest le SupertAdmin !!! '
+        ]);
+       
+    }
+        elseif($user->getStatus()=="Activer"){
+            $user->setStatus("bloquer");
+            $entityManager->flush();
+            $data = [
+                'statu' => 200,
+                'messages' => 'utilisateur  bloquer'
+            ];
+            return new JsonResponse($data);
+        }
+        
+        else{
+            $user->setStatus("Activer");
+            $entityManager->flush();
+            $data = [
+                'status' => 200,
+                'message' => 'utilisateur debloquer'
+            ];
+            return new JsonResponse($data);
+        }
+    }
+//--------------------Affectation de compte a un user------------------------////
+/**
+ * @Route("/afectcompt/{id}" , name="afectationCompt", methods={"POST"})
+ * @IsGranted("ROLE_ADMIN")
+ */
+
+public function afectcompt(Request $request, UserRepository $userRepo,EntityManagerInterface $entityManager, User $user): Response
+{
+
+    $values=$request->request->all();
+
+
+$uses = new Compts();
+$entityManager = $this->getDoctrine()->getManager();
+$form = $this->createForm(ComptuserType::class, $uses);
+        $form->handleRequest($request);
+         
+        $form->submit($values);
+$compte = $entityManager->getRepository(Compts::class)->findOneBy([
+    'numcompt'=>$values['numcompt'],
+    ]);  
+$part=$compte->getPartenaire();
+$partAdmin=$this->getUser()->getPartenaire();
+if($partAdmin != $part){
+    return new Response('Le compte n appartient pas a votre entreprise',Response::HTTP_CREATED);
+
+}
+
+   $user->setNumCompt($compte);
+   $entityManager->flush();
+   $data = [
+    'status' => 200,
+    'message' => 'afectation réussis'
+];
+return new JsonResponse($data);
+
+
+}
+  /**
+     * @Route("/findcompt", name="findcompt",methods={"POST"})
+     * @IsGranted({"ROLE_SUPERADMIN", "ROLE_ADMIN"})
+     * 
+     * 
+     */
+    public function findcompt(Request $request,ComptsRepository $comptRepository , SerializerInterface $serializer): Response
+    {
+        $values =json_decode($request->getContent(),true);
+        $listecompt= $comptRepository->findOneBy(['numcompt' => $values]);
+        $data = $serializer->serialize($listecompt, 'json', [
+            'groups' => ['find']
+        ]);
+        return new Response($data, 200, [
+
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+}
---
+++ src/Entity/Transaction.php
@@ -0,0 +1,302 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TransactionRepository")
+ */
+
+class   Transaction
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Groups({"list"})
+     */
+    private $id;
+
+    
+
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * 
+     */
+    private $cni;
+
+    /**
+     * @ORM\Column(type="bigint", nullable=true)
+     * @Groups({"list"})
+     */
+    private $montant;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateEnvoie;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $codeEnvoie;
+
+  
+
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateRetrait;
+
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $commissionEtat;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $commissionAdmin;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $commissionRetrait;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $commissionEnvoie;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="transactions")
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Agence;
+
+    
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Retrait", inversedBy="transactions")
+     * @Groups({"list"})
+     */
+    private $retrait;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Envoie", inversedBy="transactions")
+     */
+    private $envoie;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Tarifs", inversedBy="transactions")
+     */
+    private $tarif;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+  
+
+
+    public function getCni(): ?string
+    {
+        return $this->cni;
+    }
+
+    public function setCni(?string $cni): self
+    {
+        $this->cni = $cni;
+
+        return $this;
+    }
+
+    public function getMontant(): ?int
+    {
+        return $this->montant;
+    }
+
+    public function setMontant(?int $montant): self
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+
+    public function getDateEnvoie(): ?\DateTimeInterface
+    {
+        return $this->dateEnvoie;
+    }
+
+    public function setDateEnvoie(?\DateTimeInterface $dateEnvoie): self
+    {
+        $this->dateEnvoie = $dateEnvoie;
+
+        return $this;
+    }
+
+    public function getCodeEnvoie(): ?string
+    {
+        return $this->codeEnvoie;
+    }
+
+    public function setCodeEnvoie(string $codeEnvoie): self
+    {
+        $this->codeEnvoie = $codeEnvoie;
+
+        return $this;
+    }
+
+ 
+
+    public function getDateRetrait(): ?\DateTimeInterface
+    {
+        return $this->dateRetrait;
+    }
+
+    public function setDateRetrait(?\DateTimeInterface $dateRetrait): self
+    {
+        $this->dateRetrait = $dateRetrait;
+
+        return $this;
+    }
+
+   
+
+    
+
+    public function getCommissionEtat(): ?int
+    {
+        return $this->commissionEtat;
+    }
+
+    public function setCommissionEtat(int $commissionEtat): self
+    {
+        $this->commissionEtat = $commissionEtat;
+
+        return $this;
+    }
+
+    public function getCommissionAdmin(): ?int
+    {
+        return $this->commissionAdmin;
+    }
+
+    public function setCommissionAdmin(int $commissionAdmin): self
+    {
+        $this->commissionAdmin = $commissionAdmin;
+
+        return $this;
+    }
+
+    public function getCommissionRetrait(): ?int
+    {
+        return $this->commissionRetrait;
+    }
+
+    public function setCommissionRetrait(int $commissionRetrait): self
+    {
+        $this->commissionRetrait = $commissionRetrait;
+
+        return $this;
+    }
+
+    public function getCommissionEnvoie(): ?int
+    {
+        return $this->commissionEnvoie;
+    }
+
+    public function setCommissionEnvoie(int $commissionEnvoie): self
+    {
+        $this->commissionEnvoie = $commissionEnvoie;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getAgence(): ?string
+    {
+        return $this->Agence;
+    }
+
+    public function setAgence(string $Agence): self
+    {
+        $this->Agence = $Agence;
+
+        return $this;
+    }
+
+    public function getEnvoie(): ?Envoie
+    {
+        return $this->envoie;
+    }
+
+    public function setEnvoie(?Envoie $envoie): self
+    {
+        $this->envoie = $envoie;
+
+        return $this;
+    }
+
+    public function getRetrait(): ?Retrait
+    {
+        return $this->retrait;
+    }
+
+    public function setRetrait(?Retrait $retrait): self
+    {
+        $this->retrait = $retrait;
+
+        return $this;
+    }
+
+    public function getTarif(): ?Tarifs
+    {
+        return $this->tarif;
+    }
+
+    public function setTarif(?Tarifs $tarif): self
+    {
+        $this->tarif = $tarif;
+
+        return $this;
+    }
+}
